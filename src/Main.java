6bf2a6c56c69df8cb6cf0df608cc0ba153149f14
Tong ZHAN <zhantong1994@163.com>
2018-10-21 10:48:53 +0800

    测试使用一连串图片作为输入

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,12 @@
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import org.jcodec.api.FrameGrab;
-import org.jcodec.api.JCodecException;
-import org.jcodec.common.io.NIOUtils;
-import org.jcodec.common.model.Picture;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.concurrent.LinkedBlockingQueue;
 
 public class Main {
@@ -20,40 +19,24 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        BarcodeFormat barcodeFormat = BarcodeFormat.fromString(root.get("barcodeFormat").getAsString());
         ShiftCodeMLStream shiftCodeMLStream = new ShiftCodeMLStream();
         shiftCodeMLStream.configureBarcode(root);
         LinkedBlockingQueue<RawImage> queue = shiftCodeMLStream.getQueue();
         Thread thread = new Thread() {
             public void run() {
-                File file = new File("1684.mov");
-                FrameGrab grab = null;
-                try {
-                    grab = FrameGrab.createFrameGrab(NIOUtils.readableChannel(file));
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } catch (JCodecException e) {
-                    e.printStackTrace();
-                }
-                Picture picture = null;
-                int count = 0;
-                while (true) {
+                File directory = new File("/Users/zhantong/Desktop/Screen-Camera-Backend-Preprocess/VID_20180913_155407");
+                File[] images = directory.listFiles();
+                Arrays.sort(images);
+                for (File image : images) {
+                    System.out.println(image.getName());
                     try {
-                        picture = grab.getNativeFrame();
+                        BufferedImage frame = ImageIO.read(image);
+                        try {
+                            queue.put(new RawImage(frame));
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
                     } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    if (picture == null) {
-                        break;
-                    }
-                    byte[][] pictureData = picture.getData();
-                    byte[] data = new byte[pictureData[0].length + pictureData[1].length + pictureData[2].length];
-                    System.arraycopy(pictureData[0], 0, data, 0, pictureData[0].length);
-                    System.arraycopy(pictureData[1], 0, data, pictureData[0].length, pictureData[1].length);
-                    System.arraycopy(pictureData[2], 0, data, pictureData[0].length + pictureData[1].length, pictureData[2].length);
-                    try {
-                        queue.put(new RawImage(data, picture.getWidth(), picture.getHeight(), RawImage.COLOR_TYPE_YUV));
-                    } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
                 }
